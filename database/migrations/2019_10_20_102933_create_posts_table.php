2ff61e4ebca865c1940aa0f061b679eec76ce563
saidtechnology <saidx5@gmail.com>
2019-10-20 12:02:00 +0100

    add Post model with migration and controller

---
+++ database/migrations/2019_10_20_102933_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('content');
+            $table->dateTime('date_written');
+            $table->string('featured_image')->nullable();
+            $table->integer('votes_up')->nullable();
+            $table->integer('votes_down')->nullable();
+            // RelationShips
+            $table->integer('user_id');
+            $table->integer('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
