c39bf83d5532c35df19a9cbc603cfe603d176d88
saidtechnology <saidx5@gmail.com>
2019-10-20 22:14:41 +0100

    add request API

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('users', function () {
+    return \App\User::all();
+});
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
