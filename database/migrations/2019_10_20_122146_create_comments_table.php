71b9cc98ded5104cb8465e8eb2511d5711f281bd
saidtechnology <saidx5@gmail.com>
2019-10-20 14:52:40 +0100

    add Comment model with migration and controller

---
+++ database/migrations/2019_10_20_122146_create_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('content');
+            $table->dateTime('date_written');
+            $table->integer('user_id');
+            $table->integer('post_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
